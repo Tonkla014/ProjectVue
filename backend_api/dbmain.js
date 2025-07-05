3a145143d32655b3a1b878e06d497b464141d4d8
ton <piriyakorn014@gmail.com>
2025-07-05 11:32:57 +0700

    Test

--- backend_api/dbmain.js
+++ backend_api/dbmain.js
@@ -1,19 +1,3 @@
-// const mongoose = require("mongoose");
-// require('dotenv').config();
-// const servername = process.env.SERVERNAME;
-
-// const connectDB = async () => {
-//     console.log('SERVERNAME:', servername);
-//     try {
-//         await mongoose.connect(servername);
-//         console.log("db connect");
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// };
-
-// module.exports = {connectDB};
-
 
 const { MongoClient } = require('mongodb');
 // or as an es module:
